40bf55096bce4e64f542f1fcb11906c496383f68
"Aloy" <"aloycwl@gmail.com">
2022-05-05 18:42:22 +0800

    Withdrawal update

--- main.sol
+++ main.sol
@@ -1,9 +1,4 @@
 /*
-Staking contract 3 6 9 months
-3 Months 2% 
-6 Months 3%
-9 Months 4%
-
 Matching on Staking
 20% 15% 15%
 3 level*/
@@ -22,36 +17,39 @@ contract simpleMLM{
         uint token;
         uint dateJoined;
         uint lastClaimed;
-        uint package;
+        uint package; //3 months (2%), 6 months (3%), 9 months (4%)
     }
     mapping(address=>User)public user;
     constructor(){
         _owner=msg.sender;
         user[msg.sender].upline=msg.sender;
     }
-    function Deposit(address referral,uint amount)external payable{
+    function Deposit(address referral,uint amount,uint package)external payable{
         require(referral!=msg.sender);
         /*** TO BE REPLACED WITH USDT ADDRESS && SET APPROVAL FROM WEB3 FIRST ***/
         IERC20(_USDT).transferFrom(msg.sender,address(this),amount*1e18);
-        user[msg.sender].upline=referral==0x0000000000000000000000000000000000000000?_owner:referral;
+        (user[msg.sender].upline,user[msg.sender].package)=
+            (referral==0x0000000000000000000000000000000000000000?_owner:referral,package);
         user[msg.sender].dateJoined=user[msg.sender].lastClaimed=block.timestamp;
         enumUser.push(msg.sender);
     }
     function Staking()external{
-        // Detect every 730 hours since the date start (divided to exactly 31,536,000 seconds in a year)
-        // Update lastClaimed
-        for(uint i=0;i<enumUser.length;i++){
-            address d1=user[enumUser[i]].upline;
-            address d2=user[d1].upline;
-            address d3=user[d2].upline;
-            uint amt=user[enumUser[i]].investAmt;
-            (user[d1].USDT+=amt*1/20,user[d2].USDT+=amt*3/100,user[d3].USDT+=amt*1/50, //USDT=5,3,2 
-                user[d1].token+=amt*1/50,user[d2].token+=amt*3/20,user[d3].token+=amt/10); //Token=20,15,10
-        }
+        for(uint i=0;i<enumUser.length;i++)
+            if(user[enumUser[i]].lastClaimed>=730 hours&& //31,536,000 seconds a year=exactly 730 hours
+                user[enumUser[i]].dateJoined<=user[enumUser[i]].package*730 hours){ //Expire contract no money
+                address d1=user[enumUser[i]].upline;
+                address d2=user[d1].upline;
+                address d3=user[d2].upline;
+                uint amt=user[enumUser[i]].investAmt;
+                (user[d1].USDT+=amt*1/20,user[d2].USDT+=amt*3/100,user[d3].USDT+=amt*1/50, //USDT=5,3,2 
+                    user[d1].token+=amt*1/50,user[d2].token+=amt*3/20,user[d3].token+=amt/10); //Token=20,15,10
+                user[enumUser[i]].lastClaimed=block.timestamp;
+            }
     }
     function Withdraw(uint amount)external{
-        /*** SET WITHDRAWAL CONDITION ***/
-        IERC20(_USDT).transferFrom(address(this),msg.sender,amount*1e18);
-        IERC20AC(_TOKEN).transferFrom(address(this),msg.sender,amount*1e18);
+        if(user[msg.sender].dateJoined>user[msg.sender].package*730 hours){
+            IERC20(_USDT).transferFrom(address(this),msg.sender,amount*1e18);
+            IERC20AC(_TOKEN).transferFrom(address(this),msg.sender,amount*1e18);
+        }
     }
 }
