c696069dd35d4cd9ac92277d5d509a09e8fdccbd
"Aloysius" <"aloycwl@gmail.com">
2022-05-15 13:03:04 +0800

    Set fixed tokens

--- ERC20AC_93N.sol
+++ ERC20AC_93N.sol
@@ -1,27 +1,31 @@
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
-contract ERC20AC_93N{
+contract ERC20AC{
+    event Transfer(address indexed from,address indexed to,uint256 value);
+    event Approval(address indexed owner,address indexed spender,uint256 value);
     mapping(address=>uint)private _balances;
-    uint private constant _totalSupply=1e26;
-    constructor(){
-        _balances[address(this)]=_totalSupply;
-        transferFrom(address(this),0x15eD406870dB283E810D5885e432d315C94DD0dd,_totalSupply);
+    mapping(address=>mapping(address=>uint))private _allowances;
+    constructor(address a){
+        _balances[a]=totalSupply();
+        emit Transfer(address(this),a,totalSupply());
     }
-    event Transfer(address indexed a,address indexed b,uint c);
-    event Approval(address indexed a,address indexed b,uint c);
-    function name()external pure returns(string memory){return"93N";}
+    function name()external pure returns(string memory){return"93N Token";}
     function symbol()external pure returns(string memory){return"93N";}
-    function decimals()external pure returns(uint8){return 18;}
-    function totalSupply()external pure returns(uint){return _totalSupply;}
+    function decimals()external pure returns(uint){return 18;}
+    function totalSupply()public pure returns(uint){return 13e26;} //1.3 billion with 18 trailing decimal
     function balanceOf(address a)external view returns(uint){return _balances[a];}
-    function allowance(address a,address b)external pure returns(uint){a;b;return 0;}
-    function approve(address a,uint b)external pure returns(bool){a;b;return true;}
+    function allowance(address a,address b)external view returns(uint){return _allowances[a][b];}
+    function approve(address a,uint b)external returns(bool){
+        _allowances[msg.sender][a]=b;
+        emit Approval(msg.sender,a,b);
+        return true;
+    }
     function transfer(address a,uint b)external returns(bool){
         transferFrom(msg.sender,a,b);
         return true;
     }
     function transferFrom(address a,address b,uint c)public returns(bool){unchecked{
         require(_balances[a]>=c);
-        require(a==msg.sender);
+        require(a==msg.sender||_allowances[a][b]>=c);
         (_balances[a]-=c,_balances[b]+=c);
         emit Transfer(a,b,c);
         return true;
