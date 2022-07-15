5999decb809d88e90b30a8c246d3a965e0880e9b
"Aloysiu" <"aloycwl@gmail.com">
2022-07-15 16:36:34 +0800

    Updated

--- ERC721AC_93N.sol
+++ ERC721AC_93N.sol
@@ -1,109 +1,134 @@
 /***
 [DEPLOYMENT] CHANGE TOKEN ADDRESSES
-[QUERIES]
-1. If user redeposit before his tenure ends, can the total amount be override?
-2. Or each purchase must be in USDT and is a unique piece, if so the any limit?
-3. Must be weekly pay out? If that way I will add variables to put out
+web3 - open up 1st level first, then info only open up accordingly
 ***/
 pragma solidity>0.8.0;//SPDX-License-Identifier:None
-import"https://github.com/aloycwl/ERC_AC/blob/main/ERC721AC/ERC721AC.sol";
-interface IERC20{function transferFrom(address,address,uint)external;function testMint()external;}
-interface IPCSV2{function getAmountsOut(uint,address[]memory)external returns(uint[]memory);}
-contract ERC721AC_93N is ERC721AC{
+interface IERC721{
+    event Transfer(address indexed from,address indexed to,uint indexed tokenId);
+    event Approval(address indexed owner,address indexed approved,uint indexed tokenId);
+    event ApprovalForAll(address indexed owner,address indexed operator,bool approved);
+    function balanceOf(address)external view returns(uint);
+    function ownerOf(uint)external view returns(address);
+    function safeTransferFrom(address,address,uint)external;
+    function transferFrom(address,address,uint)external;
+    function approve(address,uint)external;
+    function getApproved(uint)external view returns(address);
+    function setApprovalForAll(address,bool)external;
+    function isApprovedForAll(address,address)external view returns(bool);
+    function safeTransferFrom(address,address,uint,bytes calldata)external;
+}
+interface IERC721Metadata{
+    function name()external view returns(string memory);
+    function symbol()external view returns(string memory);
+    function tokenURI(uint)external view returns(string memory);
+}
+interface IERC20{function transferFrom(address,address,uint)external;}
+interface ISWAP{function getPrice(address,address,uint)external view returns(uint);}
+contract ERC721AC_93N is IERC721,IERC721Metadata{
     /*
-    The status to be emitted 0-in USDT, 1-in 93N, 2-stake, 3-out
+    Emit status: 0-in USDT, 1-stake, 2-out
+    mapping _A: 0-owner, 1-usdt, 2-93n, 3-swap, 4-tech
+    Require all the addresses to get live price from PanCakeSwap
     */
     event Payout(address indexed from,address indexed to,uint amount,uint indexed status);
-    uint public Split;
-    uint private _count;
-    address[]private users;
-    /*
-    Require all the addresses to get live price from PanCakeSwap
-    And to transfer using interface directly
-    */
-    address private _USDT;
-    address private _TOKEN;
-    //address private constant _PCSV2=0xD99D1c33F9fC3444f8101754aBC46c52416550D1;
-    address private constant _TECH=0xdD870fA1b7C4700F2BD7f44238821C26f7392148;
     struct User{
         address upline;
         address[]downline;
+        uint[]packages;
+    }
+    struct Packages{
         uint wallet;
-        uint lastClaimed;
-        uint dateJoined;
+        uint deposit;
+        uint rate;
+        uint claimed;
+        uint joined;
         uint months;
-        uint totalDeposit;
+        address owner;
     }
-    mapping(address=>User)public user;
-    constructor(address _U, address _T){
-        _owner=user[msg.sender].upline=msg.sender;
-        (_USDT,_TOKEN)=(_U,_T);
+    uint public Split=1;
+    uint private _count;
+    uint[]public _counts;
+    mapping(uint=>Packages)public Pack;
+    mapping(uint=>address)private _A;
+    mapping(uint=>address)private _tokenApprovals;
+    mapping(address=>User)private user;
+    mapping(address=>mapping(address=>bool))private _operatorApprovals;
+    constructor(address _U,address _T){
+        (_A[0]=user[msg.sender].upline=msg.sender,_A[1]=_U,_A[2]=_T,
+        _A[3]=0xD99D1c33F9fC3444f8101754aBC46c52416550D1,_A[4]=0xdD870fA1b7C4700F2BD7f44238821C26f7392148);
+        /*
+        Add permanent packages for 0 and 4 to bypass payment checking
+        */
+        user[_A[0]].packages.push(0);
+        user[_A[4]].packages.push(0);
     }
-    function name()external pure override returns(string memory){return"Ninety Three N";}
-    function symbol()external pure override returns(string memory){return"93N";}
+    function supportsInterface(bytes4 a)external pure returns(bool){
+        return a==type(IERC721).interfaceId||a==type(IERC721Metadata).interfaceId;
+    }
+    function ownerOf(uint a)public view override returns(address){
+        return Pack[a].owner;
+    }
+    function owner()external view returns(address){
+        return _A[0];
+    }
+    function approve(address a,uint b)external override{
+        require(msg.sender==ownerOf(b)||isApprovedForAll(ownerOf(b),msg.sender));
+        _tokenApprovals[b]=a;
+        emit Approval(ownerOf(b),a,b);
+    }
+    function getApproved(uint a)public view override returns(address){
+        return _tokenApprovals[a];
+    }
+    function setApprovalForAll(address a,bool b)external override{
+        _operatorApprovals[msg.sender][a]=b;
+        emit ApprovalForAll(msg.sender,a,b);
+    }
+    function isApprovedForAll(address a,address b)public view override returns(bool){
+        return _operatorApprovals[a][b];
+    }
+    function safeTransferFrom(address a,address b,uint c)external override{
+        transferFrom(a,b,c);
+    }
+    function safeTransferFrom(address a,address b,uint c,bytes memory)external override{
+        transferFrom(a,b,c);
+    }
+    function name()external pure override returns(string memory){
+        return"Ninety Three N";
+    }
+    function symbol()external pure override returns(string memory){
+        return"93N";
+    }
+    function balanceOf(address a)external view override returns(uint){
+        return user[a].packages.length;
+    }
     function tokenURI(uint a)external view override returns(string memory){
-        uint total=user[_owners[a]].totalDeposit;
-        return total>=1e23?"ipfs://bafybeigjnlikmsm3mjvhx6ijk26bedd5lrvi3yfjlwgytzzj3h5ao6i57i/red.json":
-        total>=1e22?"ipfs://bafybeiaubm73azo4beh7am63wkua3zj4ijgy6n4gjc7spe3okwuxrt66t4/gold.json":
-        "ipfs://bafybeibtgqc26sv74erbgm6grtivjvfglffol4an4nvhorbv3ljgamg4uu/black.json";
+        return string(abi.encodePacked("ipfs://",
+        Pack[a].deposit>1e22?"bafybeibtgqc26sv74erbgm6grtivjvfglffol4an4nvhorbv3ljgamg4uu/black":
+        Pack[a].deposit>1e21?"bafybeiaubm73azo4beh7am63wkua3zj4ijgy6n4gjc7spe3okwuxrt66t4/gold":
+        "bafybeigjnlikmsm3mjvhx6ijk26bedd5lrvi3yfjlwgytzzj3h5ao6i57i/red",
+        ".json"));
     }
-    function balanceOf(address a)external view override returns(uint){return user[a].dateJoined>0?1:0;}
     function transferFrom(address a,address b,uint c)public override{unchecked{
         /*
-        Normal ERC721 token applies
-        User can only transfer to not an existing user
-        */
-        require(a==_owners[c]||getApproved(c)==a||isApprovedForAll(_owners[c],a));
-        require(user[b].dateJoined<1);
-        /*
-        Entire user will be duplicated to the new user where
+        Entire user will be duplicated to the new user
         The old user will be deleted
-        Enum will be updated too for each week's payout
         */
-        (_tokenApprovals[c]=address(0),_owners[c]=b,user[b]=user[a]);
-        delete user[a];
-        for(uint i=0;i<users.length;i++)if(users[i]==a){
-            users[i]=users[users.length-1];
-            users.pop();
-        }
-        emit Approval(_owners[c],b,c);
+        require(a==Pack[c].owner||getApproved(c)==a||isApprovedForAll(Pack[c].owner,a));
+        (_tokenApprovals[c],Pack[c].owner)=(address(0),b);
+        user[b].packages.push(c);
+        popPackages(a,c);
+        emit Approval(Pack[c].owner,b,c);
         emit Transfer(a,b,c);
     }}
-    function Deposit(address referral,uint amount,uint months)external{unchecked{
-        require(referral!=msg.sender);
-        require(user[referral].upline!=address(0));
-        /*
-        Connect to PanCakeSwap to get the live price
-        Issue the number of tokens in equivalent to USDT
-        Initiate new user
-        */
-        /*address[]memory pair=new address[](2); 
-        (pair[0],pair[1])=(_TOKEN,_USDT);
-        uint[]memory currentPrice=IPCSV2(_PCSV2).getAmountsOut(amount,pair);
-        (uint tokens,User storage u)=(amount/currentPrice[0],user[msg.sender]);*/
-        (uint tokens,User storage u)=(amount,user[msg.sender]);
-        (u.months=months,u.wallet=tokens,u.dateJoined=u.lastClaimed=block.timestamp,u.totalDeposit+=amount);
-        /*
-        Only mint and set variable when user is new
-        Only set upline and downline when user is new
-        Add user into enumUser for counting purposes
-        */
-        if(u.upline==address(0)){
-            u.upline=referral==address(0)?_owner:referral;
-            (_owners[_count]=msg.sender,_count++);
-            user[referral].downline.push(msg.sender);
-            users.push(msg.sender);
-            emit Transfer(address(0),msg.sender,_count);
+
+    function getUplines(address d0)private view returns(address d1,address d2,address d3){
+        (d1=user[d0].upline,d2=user[d1].upline,d3=user[d2].upline);
+    }
+    function popPackages(address a,uint b)private{unchecked{
+        for(uint h=0;h<user[a].packages.length;h++)if(user[a].packages[h]==b){
+            user[a].packages[h]=user[a].packages[user[a].packages.length-1];
+            user[a].packages.pop();
         }
-        /*
-        Uplines & tech to get USDT 5%, 3%, 2% & tech 1%
-        Uplines to get tokens 5%, 10%, 15%
-        Getting uplines for payout
-        */
-        (address d1,address d2,address d3)=getUplines(msg.sender); 
-        _payment(_USDT,msg.sender,msg.sender,address(this),amount,0);
-        _payment4(_USDT,address(this),msg.sender,[d1,d2,d3,_TECH],[amount*1/20,amount*3/100,amount*1/50,amount*1/100],0);
-        _payment4(_TOKEN,address(this),msg.sender,[d1,d2,d3,address(0)],[tokens*1/20,tokens*1/10,tokens*3/20,0],1);
     }}
     function _payment(address con,address from,address usr,address to,uint amt,uint status)private{
         /*
@@ -120,83 +145,110 @@ contract ERC721AC_93N is ERC721AC{
         */
         for(uint i=0;i<4;i++){
             if(to[i]==address(0))return;
-            _payment(con,from,usr,to[i],amt[i],status);
+            if(user[to[i]].packages.length>0)_payment(con,from,usr,to[i],amt[i],status);
         }
+    }}
+
+    function Deposit(address referral,uint amount,uint months)external{unchecked{
+        require(months==3||months==6||months==9);
+        require(amount>=1e20);
+        /*
+        Get price from our own swap
+        Issue the number of tokens in equivalent to USDT
+        Initiate new user
+        */
+        _count++;
+        (uint tokens,Packages storage p)=(ISWAP(_A[3]).getPrice(_A[1],_A[2],amount),Pack[_count]);
+        (p.months=months,p.wallet=tokens,p.deposit=amount,p.owner=msg.sender,p.joined=p.claimed=block.timestamp);
+        _counts.push(_count);
+        p.rate=1; //TO BE CHANGED - e.g. num_of_tokens / amount
+        user[msg.sender].packages.push(_count);
+        emit Transfer(address(0),msg.sender,_count);
+        /*
+        Only set upline and downline when user is new
+        If no address, referral is not existing member or referral is ownself, set referral as admin
+        */
+        if(user[msg.sender].upline==address(0)){
+            referral=referral==address(0)||user[referral].upline==address(0)||referral==msg.sender?_A[0]:referral;
+            user[msg.sender].upline=referral;
+            user[referral].downline.push(msg.sender);
+        }
+        /*
+        Uplines & tech to get USDT 5%, 3%, 2% & tech 1%
+        USDT to be prorated according to months
+        Getting uplines for payout
+        */
+        (address d1,address d2,address d3)=getUplines(msg.sender); 
+        _payment(_A[1],msg.sender,msg.sender,address(this),amount,0);
+        amount=amount*months/9;
+        _payment4(_A[1],address(this),msg.sender,[d1,d2,d3,_A[4]],[amount/20,amount*3/100,amount/50,amount/100],0);
     }}
     function Staking()external{unchecked{
         /*
-        Go through every users and pay them and their upline accordingly
-        31,536,000 seconds a year=exactly 730 hours
-        Get last claim and time joined to accurately payout
+        Go through every contract and pay them and their upline accordingly
+        2628e3 seconds a month
         */
-        for(uint i=0;i<users.length;i++){
-            address d0=users[i];
-            (uint timeClaimed,uint timeJoined,uint wallet)=
-            (block.timestamp-user[d0].lastClaimed,block.timestamp-user[d0].dateJoined,user[msg.sender].wallet);
-            /*
-            As long as user still in contract
-            Pro-rated payment in case this function is being called more than once a week
-            Token payment direct to wallet in term of 15%, 10%, 5%
-            Update user last claim if claimed
-            */
-            if(timeJoined<(user[d0].months+1)*730 hours){
-                if(timeClaimed>=1 hours){
-                    uint amt=timeClaimed/730*user[d0].wallet*(user[d0].months==3?2:user[d0].months==6?3:4)/100;
-                    (address d1,address d2,address d3)=getUplines(d0); 
-                    _payment4(_TOKEN,address(this),d0,[d1,d2,d3,d0],[amt*1/20,amt*1/10,amt*3/20,amt],2);
-                    user[d0].lastClaimed=block.timestamp;
+        for(uint j=0;j<_counts.length;j++){
+            (uint i,uint s)=(_counts[j],1);
+            Packages memory p=Pack[i];
+            if(p.wallet>0){
+                (address d0,uint expiry,uint amt,uint prm)=(p.owner,p.joined+p.months*2628e3,0,1);
+                (address d1,address d2,address d3)=getUplines(msg.sender); 
+                /*
+                Token payment direct to wallet in term of 15%, 10%, 5%
+                Update user's last claim if claimed
+                */
+                if(expiry>p.claimed)(amt=((expiry>block.timestamp?block.timestamp:expiry)-p.claimed)*
+                    p.wallet*(p.months/3+1)/2628e5,Pack[i].claimed=block.timestamp);
+                else{
+                    /*
+                    Contract auto expire upon due, getting amount from deposit x rate
+                    Release 34%,34%,32% and split if set
+                    Delete the contract upon last payment
+                    */
+                    (amt,prm,s)=(p.deposit*p.rate*17/50/Split,p.months/9,2);
+                    if(amt>=p.wallet){
+                        amt=p.wallet;
+                        delete Pack[i];
+                        popPackages(p.owner,i);
+                        emit Transfer(p.owner,address(0),i); 
+                    }else Pack[i].wallet-=amt;
                 }
-            /*
-            If user decided not to continue
-            Release to 4-3-3 in month
-            Months are divided if split is modified
-            */
-            }else if(wallet>0){
-                if(timeJoined>=(user[d0].months+3*Split)*730 hours)wallet=wallet/Split;
-                else wallet*=wallet*2/5/Split;
-                user[d0].wallet-=wallet;
-                _payment(_TOKEN,address(this),address(this),d0,wallet,3);
+                _payment4(_A[2],address(this),d0,[d0,d1,d2,d3],[amt,amt/20*prm,amt/10*prm,amt*3/20*prm],s);
             }
+        }
+    }}
+    function Cleanup()external{unchecked{
+        uint len=_counts.length;
+        for(uint i=0;i<_counts.length;i++)if(Pack[_counts[i]].wallet==0){
+            (_counts[i]=_counts[len-1],len--,i--);
+            _counts.pop();
         }
     }}
     function SetSplit(uint num)external{
         /*
         Modifying the split to slow down the withdrawal
         */
-        require(msg.sender==_owner);
+        require(msg.sender==_A[0]);
         Split=num;
-    }
-    function getUplines(address a)private view returns(address d1,address d2,address d3){
-        /*
-        Get direct first
-        Use previous direct to get next direct and so on
-        */
-        (d1=user[a].upline,d2=user[d1].upline,d3=user[d2].upline);
-    }
-    function getDownlines(address a)external view returns(address[]memory b,address[]memory c,address[]memory d){unchecked{
-        uint d2Length;
-        uint d3Length;
-        b=user[a].downline;
+    } 
+    function getDownlines(address a)external view returns(address[]memory lv1,uint lv2,uint lv3){unchecked{
+        lv1=user[a].downline;
         /*
         Loop through all level 2 and level 3 downlines
         Create new array counts
         Set length and reset variables for later use
         */
-        for(uint i=0;i<b.length;i++){
-            address[]memory c1=user[b[i]].downline;
-            d2Length+=c1.length;
-            for(uint j=0;j<c1.length;j++)d3Length+=user[c1[j]].downline.length;
-        }
-        (c=new address[](d2Length),d=new address[](d3Length),d2Length=d3Length=0);
-        /*
-        Fill the count with actual address
-        */
-        for(uint i=0;i<b.length;i++){
-            address[]memory c1=user[b[i]].downline;
-            for(uint j=0;j<c1.length;j++){
-                (c[d2Length]=c1[j],d2Length++);
-                for(uint k=0;k<user[c1[j]].downline.length;k++)(d[d3Length]=user[c1[j]].downline[k],d3Length++);
-            }
+        for(uint i=0;i<lv1.length;i++){
+            address[]memory c1=user[lv1[i]].downline;
+            lv2+=c1.length;
+            for(uint j=0;j<c1.length;j++)lv3+=user[c1[j]].downline.length;
         }
     }}
+    function getUserPackages(address a)external view returns(uint[]memory){
+        return user[a].packages;
+    }
 }
+
+// 1000000000000000000000
+// 0x0000000000000000000000000000000000000000
--- SwapExample.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity>0.8.0;//SPDX-License-Identifier:None
-contract SwapExample{
-    function getAmountsOut(uint a,address[]memory)external pure returns(uint[]memory c){
-        c=new uint[](2);
-        c[0]=a/10;
-    }
-}
